9c86b00a7bd3d608fc1a4846d7efe4d2d478947c
Jose Montero <josegmp20@gmail.com>
2020-11-14 14:09:41 -0500

    operacion terminada con las cuatro operaciones

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,29 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText num1, num2;
     private TextView resultado;
+    private Spinner combo;
+    private String opciones[];
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //captura objetos
         num1 = findViewById(R.id.txtNumeroUno);
         num2 = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.lblResultado);
-
+        combo = findViewById(R.id.cmbOperaciones);
+        //traemos operaciones array
+        opciones = getResources().getStringArray(R.array.operaciones);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,opciones);
+        combo.setAdapter(adapter);
     }
 
     public void calcularResultado(View v){
         if(validar()) {
-            double n1, n2, suma;
+            double n1, n2,res =0;
             n1 = Double.parseDouble(num1.getText().toString());
             n2 = Double.parseDouble(num2.getText().toString());
-            suma = n1 + n2;
-            resultado.setText(String.valueOf(suma));
+            int op = combo.getSelectedItemPosition();
+            switch (op){
+                case 0:
+                    res = n1 + n2;
+                    break;
+                case 1:
+                    res = n1-n2;
+                    break;
+                case 2:
+                    res = n1*n2;
+                    break;
+                case 3:
+                    res = n1/n2;
+                    break;
+
+
+            }
+
+            resultado.setText(String.format("%.2f",res));
         }
     }
 
@@ -43,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
+        double n2 = Double.parseDouble(num2.getText().toString());
+        int op = combo.getSelectedItemPosition();
+        if(op == 3 &&  n2 == 0){
+            num2.setError(getString(R.string.mensaje_error_division_cero));
+            num2.requestFocus();
+            return false;
+        }
         return true;
     }
 
@@ -51,5 +86,6 @@ public class MainActivity extends AppCompatActivity {
         num2.setText("");
         resultado.setText("");
         num1.requestFocus();
+        combo.setSelection(0);
     }
 }
